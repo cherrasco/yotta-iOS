f3f2ac6b3627ecb99c08d49813813f5eab5b0937
Mulyu <mulyu@hotmail.co.jp>
2016-01-30 17:47:37 +0900

    animate yotta button

--- Yotta/View/YotterView.swift
+++ Yotta/View/YotterView.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class YotterView: UIView {
 
+    @IBOutlet weak var yottaLabel: UILabel!
     @IBOutlet weak var yottaButton: UIButton!
     
+    @IBOutlet weak var yottaMargin: NSLayoutConstraint!
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -22,5 +24,16 @@ class YotterView: UIView {
 
     func animateYottaTouch() {
         
+        self.yottaMargin.constant = 20
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.yottaLabel.alpha = 1.0
+            self.layoutIfNeeded()
+            }) { result -> Void in
+                UIView.animateWithDuration(0.2, animations: { () -> Void in
+                    self.yottaLabel.alpha = 0.0
+                    }) { result -> Void in
+                        self.yottaMargin.constant = 5
+                }
+        }
     }
 }
