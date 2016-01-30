25d376536ad8d5a395a4033d1a0b1c02d0aa7520
Mulyu <mulyu@hotmail.co.jp>
2016-01-30 17:19:42 +0900

    generate YotterView

---
+++ Yotta/View/YotterView.swift
@@ -0,0 +1,26 @@
+//
+//  YotterView.swift
+//  Yotta
+//
+//  Created by mulyu on 2016/01/30.
+//  Copyright © 2016年 cherrasco. All rights reserved.
+//
+
+import UIKit
+
+class YotterView: UIView {
+
+    @IBOutlet weak var yottaButton: UIButton!
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    func animateYottaTouch() {
+        
+    }
+}
