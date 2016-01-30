7b027fa7bcc9167862ea0ffed57f166cd1f129ef
Mulyu <mulyu@hotmail.co.jp>
2016-01-30 16:51:19 +0900

    generate vc yotter and driver

---
+++ Yotta/ViewController/DriverViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DriverViewController.swift
+//  Yotta
+//
+//  Created by mulyu on 2016/01/30.
+//  Copyright © 2016年 cherrasco. All rights reserved.
+//
+
+import UIKit
+
+class DriverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Yotta/ViewController/YotterViewController.swift
@@ -0,0 +1,40 @@
+//
+//  YotterViewController.swift
+//  Yotta
+//
+//  Created by mulyu on 2016/01/30.
+//  Copyright © 2016年 cherrasco. All rights reserved.
+//
+
+import UIKit
+
+class YotterViewController: UIViewController {
+
+    struct Tag {
+        enum Button:Int {
+            case Yotta = 1
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonTouchUpInside(sender: UIButton) {
+        
+        switch sender.tag {
+        case Tag.Button.Yotta.rawValue:
+            Yotter.sharedInstance.postYotta()
+        default :
+            break
+        }
+    }
+    
+}
